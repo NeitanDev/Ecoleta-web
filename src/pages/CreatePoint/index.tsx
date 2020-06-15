b3eb2ecc1bad8206c5fb4c3e2750602033f17b8c
NeitanDev <natan.miranda02@gmail.com>
2020-06-14 22:17:16 -0300

    não sei

--- src/pages/CreatePoint/index.tsx
+++ src/pages/CreatePoint/index.tsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 import { Map, TileLayer, Marker } from 'react-leaflet';
 import axios from 'axios';
+import { LeafletMouseEvent } from 'leaflet';
 import api from '../../services/api';
 
 import './styles.css';
@@ -56,6 +57,9 @@ const CreatePoit = () => {
 
             setMunicipios(unicipios);
         })
+    }
+
+    async function handleMapClick(event: LeafletMouseEvent) {
 
     }
 
@@ -108,7 +112,7 @@ const CreatePoit = () => {
                         <span>Selecione o ponto no mapa</span>
                     </legend>
 
-                    <Map center={[-23.2577054, -46.5896002]} zoom={15}>
+                    <Map center={[-23.2577054, -46.5896002]} zoom={15} onClick={handleMapClick}>
                         <TileLayer
                             attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -130,7 +134,7 @@ const CreatePoit = () => {
                         </div>
                         <div className="field">
                             <label htmlFor="city">Cidade</label>
-                            <select name="city" id="city" onChange={(e) => { setSelectedMunicipio(e.target.value); }}>
+                            <select name="city" id="city" onChange={(e) => { setSelectedMunicipio(e.target.value); console.log(selectedMunicipio); }}>
                                 <option value="0">Selecione uma Cidade</option>
                                 {municipios.map(nome => (
                                     <option key={nome} value={nome}>{nome}</option>
@@ -148,7 +152,6 @@ const CreatePoit = () => {
 
                     <ul className="items-grid">
                         {items.map(item => (
-
                             <li key={item.id} >
                                 <img src={item.image_url} alt="teste" />
                                 <span>{item.title}</span>
